cd940d8cd75e6520d48d850a9735754f70f86893
Chavez <andre.chavez@us.ajibio-pharma.com>
2020-02-24 09:56:42 -0800

    Changed server.js to handle SPA

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+if(process.env.NODE_ENV === 'production'){
+
+    //Static Folder
+    app.use(express.static(__dirname + "/public"));
+
+    //Handle SPA
+    app.get(/.*/, (req,res) => res.sendFile(__dirname + "/public/index.html"));
+
+}
 const port = process.env.PORT || 8080
 app.listen(port)
 
